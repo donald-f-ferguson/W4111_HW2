36cf6d9ba7d63a1cc330c780d7aecb0dd0b49e97
donaldferguson <donff2@aol.com>
2019-02-12 18:31:18 -0500

    Initial project structure.

--- aeneid.py
+++ aeneid.py
@@ -1,11 +1,89 @@
 from flask import Flask
+from aeneid.dbservices import dataservice as ds
+from flask import Flask
+from flask import request
+import os
+import json
+import copy
+from aeneid.utils import utils as utils
+import re
+from aeneid.utils import webutils as wu
+from aeneid.dbservices.DataExceptions import DataException
+from flask import Response
+
+# Default delimiter to delineate primary key fields in string.
+key_delimiter = "_"
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'You probably want to go either to the content home page or call an API at /api'
+
+
+@app.route('/api')
+def api():
+    return 'You probably want to call an API on one of the resources.'
+
+
+@app.route('/api/<dbname>/<resource_name>/<primary_key>')
+def handle_resource(dbname, resource_name, primary_key):
+
+    resp = Response("Internal server error", status=500, mimetype="text/plain")
+
+    try:
+
+        key_columns = primary_key.split(key_delimiter)
+        resource = dbname + "." + resource_name
+
+        field_list = request.args.get('fields', None)
+        field_list = field_list.split(",")
+
+        result = ds.get_by_primary_key(resource, key_columns, field_list=field_list)
+
+        if result:
+            result_data = json.dumps(result, default=str)
+            resp = Response(result_data, status=200, mimetype='application/json')
+        else:
+            resp = Response("Not found", status=404, mimetype="text/plain")
+    except Exception as e:
+        utils.debug_message("Something awlful happened, e = ", e)
+
+    return resp
+
+@app.route('/api/<dbname>/<resource_name>')
+def handle_collection(dbname, resource_name):
+
+    resp = Response("Internal server error", status=500, mimetype="text/plain")
+
+    try:
+
+        resource = dbname + "." + resource_name
+
+        field_list = request.args.get('fields', None)
+        field_list = field_list.split(",")
+
+        tmp = None
+        for k,v in request.args.items():
+            if not k == 'fields':
+                if tmp is None:
+                    tmp = {}
+                tmp[k] = v
+
+        result = ds.get_by_template(resource, tmp, field_list=field_list)
+
+        if result:
+            result_data = json.dumps(result, default=str)
+            resp = Response(result_data, status=200, mimetype='application/json')
+        else:
+            resp = Response("Not found", status=404, mimetype="text/plain")
+    except Exception as e:
+        utils.debug_message("Something awlful happened, e = ", e)
+
+    return resp
+
 
 
 if __name__ == '__main__':
--- aeneid/dbservices/dataservice.py
+++ aeneid/dbservices/dataservice.py
@@ -6,6 +6,52 @@ import aeneid.utils.dffutils as db
 import aeneid.dbservices.DataExceptions
 from aeneid.dbservices.RDBDataTable import RDBDataTable
 
+db_schema = None                                # Schema containing accessed data
+cnx = None                                      # DB connection to use for accessing the data.
+key_delimiter = '_'                             # This should probably be a config option.
+
+# Is a dictionary of {table_name : [primary_key_field_1, primary_key_field_2, ...]
+# Used to convert a list of column values into a template of the form { col: value }
+primary_keys = {}
+
+# This dictionary contains columns mappings for nevigating from a source table to a destination table.
+# The keys is of the form sourcename_destinationname. The entry is a list of the form
+# [[sourcecolumn1, destinationcolumn1], ...
+join_columns = {}
+
+# Data structure contains RI constraints. The format is a dictionary with an entry for each schema.
+# Within the schema entry, there is a dictionary containing the constraint name, source and target tables
+# and key mappings.
+ri_constraints = None
+
+data_tables = {}
+
+
+def get_data_table(table_name):
+
+    result = data_tables.get(table_name, None)
+    if result is None:
+        result = RDBDataTable(table_name)
+        data_tables[table_name] = result
+
+    return result
+
+
+def get_by_template(table_name, template, field_list=None, limit=None, offset=None, order_by=None, commit=True):
+
+    dt = get_data_table(table_name)
+    result = dt.find_by_template(template, field_list, limit, offset, order_by, commit)
+    return result.get_rows()
+
+
+def get_by_primary_key(table_name, key_fields, field_list=None, commit=True):
+
+    dt = get_data_table(table_name)
+    result = dt.find_by_primary_key(key_fields, field_list)
+    return result
+
+
+
 
 
 
